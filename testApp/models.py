2b70e3346f8cc28a8fd02cb110f761d4d307e4bb
Shilpa Thakur <shithaku@adobe.com>
2018-04-30 00:15:20 +0530

    My application's second deployment

--- testApp/models.py
+++ testApp/models.py
@@ -8,7 +8,7 @@ class OverwriteStorage(FileSystemStorage):
 
     def get_available_name(self, name,max_length):
         if self.exists(name):
-            os.remove(os.path.join(settings.MEDIA_DIR, name))
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
 
 class Topics(models.Model):
