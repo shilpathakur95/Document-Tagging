be6c6d19751329377902138a53c899e477c67d83
Shilpa Thakur <shithaku@adobe.com>
2018-05-18 00:30:50 +0530

    My application's first deployment

--- testProject/settings.py
+++ testProject/settings.py
@@ -127,7 +127,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_DIR = os.path.join(BASE_DIR, 'media/uploads')
 # Static files (CSS, JavaScript, Images)
